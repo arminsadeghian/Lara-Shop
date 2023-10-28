2a3189b00bdbc289cf06b5f638d6e34195d1bb5d
Armin Sadeghian <96877864+arminsadeghian@users.noreply.github.com>
2023-10-28 13:54:41 +0330

    Add migrations

---
+++ database/migrations/2023_10_28_074913_create_user_addresses_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_addresses', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->unsignedBigInteger('province_id');
+            $table->foreign('province_id')
+                ->references('id')
+                ->on('provinces')
+                ->onDelete('cascade');
+
+            $table->unsignedBigInteger('city_id');
+            $table->foreign('city_id')
+                ->references('id')
+                ->on('cities')
+                ->onDelete('cascade');
+
+            $table->string('title');
+            $table->text('address');
+            $table->string('postal_code');
+            $table->string('long')->nullable();
+            $table->string('lat')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_addresses');
+    }
+};
