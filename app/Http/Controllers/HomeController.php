0aee4d5be56bd0f768812e7c98adcdffe9960c66
Armin Sadeghian <96877864+arminsadeghian@users.noreply.github.com>
2023-11-08 17:02:57 +0330

    Show main slider in index page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Banner;
 use App\Models\Category;
 
 class HomeController extends Controller
@@ -9,6 +10,7 @@ class HomeController extends Controller
     public function index()
     {
         $parentCategories = Category::where('parent_id', 0)->get();
-        return view('front.index', compact('parentCategories'));
+        $sliders = Banner::where('type', 'slider')->where('is_active', 1)->orderBy('priority')->get();
+        return view('front.index', compact('parentCategories', 'sliders'));
     }
 }
