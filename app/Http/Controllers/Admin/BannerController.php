19815a1eb6a5c4a02880a3d5170fff67dc825ef6
Armin Sadeghian <96877864+arminsadeghian@users.noreply.github.com>
2023-11-08 09:07:14 +0330

    Implement create new banner template to admin panel

---
+++ app/Http/Controllers/Admin/BannerController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BannerController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        return view('admin.banners.create');
+    }
+
+    public function store(Request $request)
+    {
+        dd($request->all());
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function edit(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
