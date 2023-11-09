ca773b5b189f5440ea01376e549242276e30acdc
Armin Sadeghian <96877864+arminsadeghian@users.noreply.github.com>
2023-11-09 17:42:56 +0330

    Show newest products list in index page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Banner;
 use App\Models\Category;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
@@ -12,6 +13,7 @@ class HomeController extends Controller
         $parentCategories = Category::where('parent_id', 0)->get();
         $sliders = Banner::where('type', 'slider')->where('is_active', 1)->orderBy('priority')->get();
         $indexTopBanners = Banner::where('type', 'index-top')->where('is_active', 1)->orderBy('priority')->get();
-        return view('front.index', compact('parentCategories', 'sliders', 'indexTopBanners'));
+        $products = Product::where('is_active', 1)->orderBy('created_at', 'DESC')->take(5)->get();
+        return view('front.index', compact('parentCategories', 'sliders', 'indexTopBanners', 'products'));
     }
 }
