c7459b883f4e0945a911ec9cc821ba92a907b948
Armin Sadeghian <96877864+arminsadeghian@users.noreply.github.com>
2023-12-11 13:27:12 +0330

    Update auth controller

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -12,7 +12,7 @@ class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        if ($request->method() == 'GET') {
+        if ($request->method() == 'GET' && auth()->check() == false) {
             return view('auth.login');
         }
 
